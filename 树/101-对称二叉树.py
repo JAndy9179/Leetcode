5d826cc763cc703b15f91da77fa12fb77e6eeecb
JAndy9179 <jandy9179@gmail.com>
2026-08-28 17:31:32 +0800

    add 101

---
+++ 树/101-对称二叉树.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def check(l, r):
+            if l == None and r == None:
+                return True
+            if not l or not r:
+                return False
+            if l.val != r.val:
+                return False
+            return check(l.left, r.right) and check(l.right, r.left)
+
+        return check(root.left, root.right)
