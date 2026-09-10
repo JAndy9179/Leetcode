a6c4a30e59501258b44efe8977cb6b74a29a7215
JAndy9179 <jandy9179@gmail.com>
2026-09-10 17:54:27 +0800

    edit 3

--- 数组与链表/3-无重复字符的最长子串.py
+++ 数组与链表/3-无重复字符的最长子串.py
@@ -1,18 +1,12 @@
-from collections import defaultdict
-
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_dict = defaultdict(int)
-        max_l, max_r = 0, 0
-        l = 0
-        for i, c in enumerate(s):
-            char_dict[c] += 1
-            while char_dict[c] == 2:
-                char_dict[s[l]] -= 1
-                l += 1
+        stack = []
+        max_len = 0
+        for c in s:
+            while c in stack:
+                stack.pop(0)
 
-            if i + 1 - l > max_r - max_l:
-                max_l, max_r = l, i + 1
+            stack.append(c)
+            max_len = max(max_len, len(stack))
 
-        return max_r - max_l
+        return max_len
